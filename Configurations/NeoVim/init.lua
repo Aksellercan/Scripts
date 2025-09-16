dfb85f2f913e8d973bbdeeb9959fb73efd13df88
Aksellercan <akselercan2003@gmail.com>
2025-09-16 19:08:53 +0300

    nvim update

--- Configurations/NeoVim/init.lua
+++ Configurations/NeoVim/init.lua
@@ -1,3 +1,4 @@
 require("cloudberrynvim")
 require("config.lazy")
+require("config.mini")
 print("hello")
--- Configurations/NeoVim/lua/config/lazy.lua
+++ Configurations/NeoVim/lua/config/lazy.lua
@@ -24,9 +24,9 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
+	{ 'nvim-mini/mini.statusline', version = false },
     -- import your plugins
     { import = "plugins" },
-
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
